9d3cf6f16d72a1cba7e9ab1520ee1622273825b8
albi <smartety@gmail.com>
2023-11-22 23:49:06 +0600

    all ok

--- index.php
+++ index.php
@@ -23,12 +23,20 @@
 		$user =  $_POST['user'];
 
 
+
+
 		//Check email
 		if( isset($email)){
 			$email_arra = explode('@', $email);
 			$insta_mail = end($email_arra);
 		}
-		
+
+
+		//Check number oparator 
+
+		 $cell_start = substr( $cell, 0, 3);
+
+
 
 
 		if ( empty($name)){
@@ -49,15 +57,19 @@
 		 */
 
 		if( empty($name) || empty($email) || empty($cell) || empty($user)){
-
 			$msg = "<p class=\" alert alert-danger\">All feilds are required!!! <button class=\"close\" data-dismiss=\"alert\">&times;</button></p>";
 
 		}else if( $insta_mail != 'albiak47.com'){
 			$msg = "<p class=\" alert alert-info\">You should have a albiak47.com email to regiter!  <button class=\"close\" data-dismiss=\"alert\">&times;</button></p>";
+
+		} else if( in_array($cell_start, ['017','013','016','018','015', '019','014']) == false){
+			$msg = "<p class=\" alert alert-info\">Mobile number should be BD number<button class=\"close\" data-dismiss=\"alert\">&times;</button></p>";
+
 		}
 		else if(filter_var($email, FILTER_VALIDATE_EMAIL) == false) {
 			$msg = "<p class=\" alert alert-warning\">Invalid email address! <button class=\"close\" data-dismiss=\"alert\">&times;</button></p>";
-		}else{
+		}
+		else{
 			$msg = "<p class=\"alert alert-success\"> Data stable <button class=\"close\" data-dismiss=\"alert\">&times;</button></p>";
 		}
 
@@ -87,8 +99,8 @@
 
 				<form action="" method="POST" enctype="multipart/form-data">
 					<div class="form-group">
-						<label for="">Name</label>
-						<input name="name" class="form-control" type="text">
+						<label for="nam">Name</label>
+						<input name="name" class="form-control" type="text" id="nam">
 						<?php 
 							if( isset($err['name'])){
 								echo $err['name'];
@@ -98,17 +110,18 @@
 
 					</div>
 					<div class="form-group">
-						<label for="">Email</label>
-						<input name="email" class="form-control" type="text">
+						<label for="em">Email</label>
+						<input name="email" class="form-control" type="text" id="em">
 						<?php 
 							if( isset($err['email'])){
 								echo $err['email'];
 							}
 						?>
 					</div>
+					
 					<div class="form-group">
-						<label for="">Cell</label>
-						<input name="cell" class="form-control" type="text">
+						<label for="ci">Cell</label>
+						<input name="cell" class="form-control" type="text" id="ci">
 						<?php 
 							if( isset($err['cell'])){
 								echo $err['cell'];
@@ -116,13 +129,17 @@
 						?>
 					</div>
 					<div class="form-group">
-						<label for="">Username</label>
-						<input name="user" class="form-control" type="text">
+						<label for="us">Username</label>
+						<input name="user" class="form-control" type="text" id="us">
 						<?php 
 							if( isset($err['user'])){
 								echo $err['user'];
 							}
 						?>
+					</div>
+					<div class="form-group">
+						<label for="up"><img width="40" height="40" src="file.png" alt=""></label>
+						<input style="display:none;" type="file" name="c_file" id="up">
 					</div>
 					<div class="form-group">
 						<input name="submit" class="btn btn-primary" type="submit" value="Sign Up">
